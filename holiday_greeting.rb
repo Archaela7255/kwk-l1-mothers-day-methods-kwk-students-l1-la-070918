1e2471ab211f96d71b008f9a7c2d3ae1122ccc89
Archaela7255 <193241@students.avhsd.org>
2018-07-10 20:40:10 +0000

    automatically backed up by learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,5 @@
+def holiday_greeting(recievers_name = "Mom", senders_name = "Your Favorite Child", holiday = "Mothers Day")
+  puts "Happy #{holiday}, #{recievers_name}! From #{senders_name}"
+end 
+
+holiday_greeting
