b2deeef843c8ba714669a26ab886357070da1328
Archaela7255 <193241@students.avhsd.org>
2018-07-10 20:41:13 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -2,4 +2,4 @@ def holiday_greeting(recievers_name = "Mom", senders_name = "Your Favorite Child
   puts "Happy #{holiday}, #{recievers_name}! From #{senders_name}"
 end 
 
-holiday_greeting
+holiday_greeting("Beyonce", "Jay-Z", "Fourth of July")
